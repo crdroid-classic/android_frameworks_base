4da344dd5e9c8493e2405ff0c93740b93c3a4ce4
Miranda Kephart <mkephart@google.com>
2020-11-16 11:22:13 -0500

    Close screenshot process on user switched
    Currently, we keep the process up even if the user switches,
    meaning that in some cases (if the user is switched while the
    screenshot UI is up) we will save images to the wrong profile.
    This change makes ScreenshotHelper listen for user switches and
    close the screenshot service, so that a new screenshot is
    guaranteed to be constructed with the correct user's context.
    
    Bug: 170474245
    Fix: 170474245
    Test: manual -- verified bad state occurs if user switches within
    the timeout period, ensured that screenshots work immediately
    after switching with this change.
    
    Change-Id: I9d32d0928e6c2bda161d04555438d0dd7afef0ba
    (cherry picked from commit 7ef1a5dd1506075507412626f2533283d9520144)
    (cherry picked from commit 8a2656d4b69efcec5140f3de5a9a721ffb21112e)

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6749,8 +6749,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         @Override public void run() {
             synchronized (mScreenshotLock) {
                 if (mScreenshotConnection != null) {
-                    mContext.unbindService(mScreenshotConnection);
-                    mScreenshotConnection = null;
+                    resetScreenshotConnection();
                     notifyScreenshotError();
                 }
             }
@@ -6782,8 +6781,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             public void handleMessage(Message msg) {
                                 synchronized (mScreenshotLock) {
                                     if (mScreenshotConnection == myConn) {
-                                        mContext.unbindService(mScreenshotConnection);
-                                        mScreenshotConnection = null;
+                                        resetScreenshotConnection();
                                         mHandler.removeCallbacks(mScreenshotTimeout);
                                     }
                                 }
@@ -6806,8 +6804,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 public void onServiceDisconnected(ComponentName name) {
                     synchronized (mScreenshotLock) {
                         if (mScreenshotConnection != null) {
-                            mContext.unbindService(mScreenshotConnection);
-                            mScreenshotConnection = null;
+                            resetScreenshotConnection();
                             mHandler.removeCallbacks(mScreenshotTimeout);
                             notifyScreenshotError();
                         }
@@ -6864,6 +6861,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             default:
                 Log.w(TAG, "getVolumeWakeTriggered: unexpected keyCode=" + keyCode);
                 return false;
+        }
+    }
+
+    /**
+     * Reset the screenshot connection.
+     */
+    private void resetScreenshotConnection() {
+        if (mScreenshotConnection != null) {
+            mContext.unbindService(mScreenshotConnection);
+            mScreenshotConnection = null;
         }
     }
 
@@ -7713,6 +7720,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 synchronized (mWindowManagerFuncs.getWindowManagerLock()) {
                     mLastSystemUiFlags = 0;
                     updateSystemUiVisibilityLw();
+                }
+
+                // close the screenshot connection on user switch so that screenshots
+                // are always started with the correct user's context
+                synchronized(mScreenshotLock) {
+                  resetScreenshotConnection();
                 }
             }
         }
